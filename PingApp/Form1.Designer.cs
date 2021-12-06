1f403d4bfef1727b7277bcb3b6d52df6cde10e4f
Scott Durkin <scottdurkin97@gmail.com>
2021-12-06 22:45:40 +0000

    Add project files.

---
+++ PingApp/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace PingApp
+{
+    partial class IDD_FORM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IDE_PING_LIST = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // IDE_PING_LIST
+            // 
+            this.IDE_PING_LIST.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.IDE_PING_LIST.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.IDE_PING_LIST.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.IDE_PING_LIST.FormattingEnabled = true;
+            this.IDE_PING_LIST.ItemHeight = 20;
+            this.IDE_PING_LIST.Location = new System.Drawing.Point(13, 13);
+            this.IDE_PING_LIST.Name = "IDE_PING_LIST";
+            this.IDE_PING_LIST.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.IDE_PING_LIST.Size = new System.Drawing.Size(354, 360);
+            this.IDE_PING_LIST.TabIndex = 0;
+            // 
+            // IDD_FORM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(379, 453);
+            this.Controls.Add(this.IDE_PING_LIST);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "IDD_FORM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pinger";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox IDE_PING_LIST;
+    }
+}
+
---
+++ PingApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingApp
+{
+    public partial class IDD_FORM : Form
+    {
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        public IDD_FORM()
+        {
+            InitializeComponent();
+
+            /* Adds the event and the event handler for the method that will 
+         process the timer event to the timer. */
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+
+            // Sets the timer interval to 5 seconds.
+            myTimer.Interval = 5000;
+            myTimer.Start();
+
+            //// Runs the timer, and raises the event.
+            //while (exitFlag == false)
+            //{
+            //    // Processes all the events in the queue.
+            //    Application.DoEvents();
+            //}
+        }
+
+        // This is the method to run when the timer is raised.
+        private void TimerEventProcessor(Object myObject, EventArgs myEventArgs)
+        {
+            myTimer.Stop();
+
+            try
+            {
+                Ping ping = new Ping();
+                PingReply reply = ping.Send("google.com");
+                if (reply != null)
+                {
+                    String msg = String.Format("Ping: {0}ms", reply.RoundtripTime);
+                    IDE_PING_LIST.Items.Add(msg);
+                    IDE_PING_LIST.SelectedIndex = IDE_PING_LIST.Items.Count - 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            myTimer.Enabled = true;
+        }
+    }
+}
