9da6a6777e5d7666f1b64f86b56d2e7867596573
Scott Durkin <scottdurkin97@gmail.com>
2021-12-07 11:45:41 +0000

    Ping Counter and Last Ping date/time

--- PingApp/Form1.Designer.cs
+++ PingApp/Form1.Designer.cs
@@ -30,20 +30,47 @@ namespace PingApp
         private void InitializeComponent()
         {
             this.IDE_PING_LIST = new System.Windows.Forms.ListBox();
+            this.IDC_PING_COUNT = new System.Windows.Forms.Label();
+            this.IDC_LAST_PING_DATETIME = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // IDE_PING_LIST
             // 
             this.IDE_PING_LIST.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.IDE_PING_LIST.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.IDE_PING_LIST.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.IDE_PING_LIST.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.IDE_PING_LIST.FormattingEnabled = true;
-            this.IDE_PING_LIST.ItemHeight = 20;
+            this.IDE_PING_LIST.ItemHeight = 22;
             this.IDE_PING_LIST.Location = new System.Drawing.Point(13, 13);
             this.IDE_PING_LIST.Name = "IDE_PING_LIST";
             this.IDE_PING_LIST.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.IDE_PING_LIST.Size = new System.Drawing.Size(354, 360);
+            this.IDE_PING_LIST.Size = new System.Drawing.Size(354, 352);
             this.IDE_PING_LIST.TabIndex = 0;
+            // 
+            // IDC_PING_COUNT
+            // 
+            this.IDC_PING_COUNT.AutoSize = true;
+            this.IDC_PING_COUNT.BackColor = System.Drawing.Color.Transparent;
+            this.IDC_PING_COUNT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDC_PING_COUNT.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.IDC_PING_COUNT.Location = new System.Drawing.Point(9, 380);
+            this.IDC_PING_COUNT.Name = "IDC_PING_COUNT";
+            this.IDC_PING_COUNT.Size = new System.Drawing.Size(75, 22);
+            this.IDC_PING_COUNT.TabIndex = 1;
+            this.IDC_PING_COUNT.Text = "Pings: 0";
+            // 
+            // IDC_LAST_PING_DATETIME
+            // 
+            this.IDC_LAST_PING_DATETIME.AutoSize = true;
+            this.IDC_LAST_PING_DATETIME.BackColor = System.Drawing.Color.Transparent;
+            this.IDC_LAST_PING_DATETIME.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDC_LAST_PING_DATETIME.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.IDC_LAST_PING_DATETIME.Location = new System.Drawing.Point(9, 404);
+            this.IDC_LAST_PING_DATETIME.Name = "IDC_LAST_PING_DATETIME";
+            this.IDC_LAST_PING_DATETIME.Size = new System.Drawing.Size(186, 22);
+            this.IDC_LAST_PING_DATETIME.TabIndex = 2;
+            this.IDC_LAST_PING_DATETIME.Text = "Last Ping: No ping yet";
             // 
             // IDD_FORM
             // 
@@ -51,6 +78,8 @@ namespace PingApp
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(379, 453);
+            this.Controls.Add(this.IDC_LAST_PING_DATETIME);
+            this.Controls.Add(this.IDC_PING_COUNT);
             this.Controls.Add(this.IDE_PING_LIST);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -58,12 +87,15 @@ namespace PingApp
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pinger";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox IDE_PING_LIST;
+        private System.Windows.Forms.Label IDC_PING_COUNT;
+        private System.Windows.Forms.Label IDC_LAST_PING_DATETIME;
     }
 }
 
--- PingApp/Form1.cs
+++ PingApp/Form1.cs
@@ -48,6 +48,16 @@ namespace PingApp
                     String msg = String.Format("Ping: {0}ms", reply.RoundtripTime);
                     IDE_PING_LIST.Items.Add(msg);
                     IDE_PING_LIST.SelectedIndex = IDE_PING_LIST.Items.Count - 1;
+
+                    DateTime date = DateTime.Now;
+
+                    String DateStr = date.ToString("dd/MM/yy");
+                    String TimeStr = date.ToString("hh:mm:sstt");
+
+                    String PingCountStr = String.Format("Pings: {0}", IDE_PING_LIST.Items.Count);
+                    String PingLastStr = String.Format("Last Ping: {0} - {1}", DateStr, TimeStr);
+                    IDC_PING_COUNT.Text = PingCountStr;
+                    IDC_LAST_PING_DATETIME.Text = PingLastStr;
                 }
             }
             catch (Exception ex)
